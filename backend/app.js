7ab22ed1100840b7b1046e86333cf285ba5ff728
333shanjose <shanjose662@gmail.com>
2026-08-21 17:28:54 +0530

    commited

--- backend/app.js
+++ backend/app.js
@@ -58,7 +58,7 @@ const upload = multer({ storage })
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://mernstck-shopease-cart-client.onrender.com',
     credentials: true,
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
